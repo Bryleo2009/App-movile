eb5322501e1e52f584aa8ad5e0d59c1d3578837b
Bryan Andrés Morán Vega <bryleo2009@hotmail.com>
2023-07-06 19:15:27 -0500

    web view

--- app/src/main/java/com/example/ofsystem/WebViewActivity.java
+++ app/src/main/java/com/example/ofsystem/WebViewActivity.java
@@ -2,11 +2,18 @@ package com.example.ofsystem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.os.Build;
 import android.os.Bundle;
 
+import android.util.Log;
 import android.view.View;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 public class WebViewActivity extends AppCompatActivity {
 
@@ -16,16 +23,24 @@ public class WebViewActivity extends AppCompatActivity {
         setContentView(R.layout.web_view);
 
         WebView webView = findViewById(R.id.webView);
-        webView.setWebViewClient(new WebViewClient());
         webView.getSettings().setJavaScriptEnabled(true);
-        webView.getSettings().setUserAgentString("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3");
-        webView.getSettings().setMediaPlaybackRequiresUserGesture(false);
-        webView.getSettings().setAllowFileAccess(true);
-        webView.getSettings().setAllowContentAccess(true);
-        webView.getSettings().setAllowFileAccessFromFileURLs(true);
-        webView.getSettings().setAllowUniversalAccessFromFileURLs(true);
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                System.out.println("WebView" + "Página cargada: " + url);
+            }
 
-        webView.loadUrl("https://drive.google.com/file/d/1Xxw8rsfcNIw4CRTib2F0xQxidDa3WMqO/view?usp=sharing");
-        webView.setVisibility(View.VISIBLE);
+            @Override
+            public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+                super.onReceivedError(view, request, error);
+                System.out.println("WebView" + "Error al cargar la página: " + error.getDescription());
+            }
+        });
+
+        String pdfUrl = "https://web-deprueba.000webhostapp.com/comprobante_N0000.pdf";
+        String pdfData = "<iframe src='http://docs.google.com/gview?embedded=true&url=" + pdfUrl + "' style='width:100%; height:100%;' frameborder='0'></iframe>";
+
+        webView.loadData(pdfData, "text/html", "utf-8");
     }
 }
